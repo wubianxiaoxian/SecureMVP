37aa1c1e47000025180cde30cf04ee99af0caf8c
KentSun <kaifeng@base64.amsterdam>
2025-11-15 10:22:45 +0800

    Initial Commit

---
+++ SecureMVP/SecureMVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureMVPApp.swift
+//  SecureMVP
+//
+//  Created by kent.sun on 2025/11/15.
+//
+
+import SwiftUI
+
+@main
+struct SecureMVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SecureMVPTests/SecureMVPTests.swift
@@ -0,0 +1,17 @@
+//
+//  SecureMVPTests.swift
+//  SecureMVPTests
+//
+//  Created by kent.sun on 2025/11/15.
+//
+
+import Testing
+@testable import SecureMVP
+
+struct SecureMVPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
